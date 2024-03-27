9cee563ca12d76af88d8dab1130d865cd4fc210a
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-03-26 23:07:14 -0400

    fixing errors

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,11 +1,11 @@
-env
-#ENV_NAME=$(echo $GO_STAGE_NAME | tr [:upper:] [:lower:] )
-#
-#aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --type "String"  --value "${app_version}"  --overwrite
-#
-#aws ec2 describe-instances --filter "Name=tag:Name,Values=${ENV_NAME}-expense-${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >/tmp/hosts
-#
-#ansible-playbook  -i  /tmp/hosts  -e  ansible_user=${ssh_user} -e  ansible_password=${ssh_pass} expense.yml -e service_name=${component} -e env=${ENV_NAME} -e app_version=${app_version}
-#
-#ssh_pass=${aws ssm get-parameter --name ssh.password --with-decryption --query 'Parameter.Value' |xargs}
-#ssh_user=${aws ssm get-parameter --name ssh.username --with-decryption --query 'Parameter.Value' |xargs}
+
+ENV_NAME=$(echo $GO_STAGE_NAME | tr [:upper:] [:lower:] )
+
+aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --type "String"  --value "${app_version}"  --overwrite
+
+aws ec2 describe-instances --filter "Name=tag:Name,Values=${ENV_NAME}-expense-${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >/tmp/hosts
+
+ansible-playbook  -i  /tmp/hosts  -e  ansible_user=${ssh_username} -e  ansible_password=${ssh_password} expense.yml -e service_name=${component} -e env=${ENV_NAME} -e app_version=${app_version}
+
+ssh_username=${aws ssm get-parameter --name ssh.username --with-decryption --query 'Parameter.Value' --output text}
+ssh_password=$(aws ssm get-parameter --name "ssh.password" --with-decryption  --query 'Parameter.Value' --output text)
