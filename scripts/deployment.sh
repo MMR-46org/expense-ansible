d4b2f9d5d43a57f41f6c8e8911b8427834da5a69
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-03-27 12:18:48 -0400

    fixing errors in deploy file

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,7 +1,7 @@
 
 ENV_NAME=$(echo $GO_STAGE_NAME | tr [:upper:] [:lower:] )
 
-aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --type "String"  --value "${app_version}"  --overwrite
+aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --value "app_version" --type "String"  --overwrite
 
 aws ec2 describe-instances --filter "Name=tag:Name,Values=${ENV_NAME}-expense-${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >/tmp/hosts
 
@@ -9,3 +9,5 @@ ansible-playbook  -i  /tmp/hosts  -e  ansible_user=${ssh_username} -e  ansible_p
 
 ssh_username=$(aws ssm get-parameter --name ssh.username --with-decryption --query 'Parameter.Value' --output text)
 ssh_password=$(aws ssm get-parameter --name ssh.password --with-decryption  --query 'Parameter.Value' --output text)
+
+
