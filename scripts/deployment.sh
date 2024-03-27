76f62f31887a8d6f1884f31a0b6e0e41d1d0ee61
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-03-26 21:59:10 -0400

    updating deployment script update the app version in parameter store

---
+++ scripts/deployment.sh
@@ -0,0 +1,5 @@
+ENV_NAME=$(echo $GO_STAGE_NAME | tr [:upper:] [:lower:])
+
+aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --type "String"  --value "${app_version}"  --overwrite
+
+#ansible-playbook  -i  backend-dev.madhanmohanreddy.tech, -e  ansible_user=centos -e  ansible_password=DevOps321 expense.yml -e service_name=${component} -e env=dev -e app_version=${app_version}
