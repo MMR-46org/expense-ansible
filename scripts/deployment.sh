939402bf55a014f5e7c983a2b4445dc2b3fa7b8d
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-03-27 12:56:50 -0400

    fixing errors in deploy file

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,13 +1,11 @@
 
 ENV_NAME=$(echo $GO_STAGE_NAME | tr [:upper:] [:lower:] )
 
-aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --value "app_version" --type "String"  --overwrite
+aws ssm put-parameter  --name "${ENV_NAME}.expense.${component}.app_version"  --value "${app_version}" --type "String"  --overwrite
 
 aws ec2 describe-instances --filter "Name=tag:Name,Values=${ENV_NAME}-expense-${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >/tmp/hosts
 
-ansible-playbook  -i  /tmp/hosts  -e  ansible_user=${ssh_username} -e  ansible_password=${ssh_password} expense.yml -e service_name=${component} -e env=${ENV_NAME} -e app_version=${app_version}
+ansible-playbook  -i  /tmp/hosts  -e  ansible_user=${ssh_user} -e  ansible_password=${ssh_pass} expense.yml -e service_name=${component} -e env=${ENV_NAME} -e app_version=${app_version}
 
-ssh_username=$(aws ssm get-parameter --name ssh.username --with-decryption --query 'Parameter.Value' --output text)
-ssh_password=$(aws ssm get-parameter --name ssh.password --with-decryption  --query 'Parameter.Value' --output text)
-
-
+ssh_user=$(aws ssm get-parameter --name ssh.username --with-decryption --query 'Parameter.Value' |xargs)
+ssh_pass=$(aws ssm get-parameter --name ssh.password --with-decryption --query 'Parameter.Value' |xargs)
